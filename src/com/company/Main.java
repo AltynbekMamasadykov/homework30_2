ec6d7ff0dbd3b96b9502795f2df862e7b8502958
Asanbai Ortosaev <asanbaiortosaev@gmail.com>
2022-03-26 01:07:41 +0600

    comments added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,13 +10,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
+
+            //---------------------- printing greeting -------------
+
             System.out.println("Welcome to basic arithmetic calculator");
             System.out.println("Давай жаз, Маалыматтар бир сапта берилиши керек! " +
                     "Ар бир сан жана арифметикалык операция жаңы сапта берилсе, " +
                     "андай чечимдер жараксыз деп эсептелет." +
                     "\nКалькулятор бир эле учурда араб же рим цифралары менен гана иштей алат");
 
+            //--------------- checking if entered line is empty ------------
+
+            Scanner scanner = new Scanner(System.in);
             String operation = scanner.nextLine();
             if(operation==null || operation.equals("")){
                 try {
@@ -27,10 +32,14 @@ public class Main {
                 }
             }
 
+            //---------------------- declaration & initialization -------------
+
             int result = 0;
             String strResult = "";
             int indexOfOperator = -1;
             String operator = "";
+
+            //-------------------- assigning operator -------------------------
 
             if(operation.contains("+")){
                 indexOfOperator = operation.indexOf('+');
@@ -46,6 +55,8 @@ public class Main {
                 operator = "/";
             }
 
+            //-------------------- checking for valid operator -------------------
+
             if(!operator.matches(".*[-*/+].*")){
                 try {
                     throw new WrongOperatorException("+ - * / эле колдонсо болот, бул арифметический калькулятор");
@@ -55,8 +66,13 @@ public class Main {
                 }
             }
 
+            //----------------- getting values of first and second operand --------
+
             String firstNumber = operation.substring(0,indexOfOperator);
             String secondNumber = operation.substring(indexOfOperator+1);
+
+
+            //---------------- calculation with roman numerals ---------------------
 
             if(firstNumber.matches("[A-Z]+") && secondNumber.matches("[A-Z]+")){
                 int intFirstNumber = RomanToArabic.convertRomanToArabic(firstNumber);
@@ -68,6 +84,8 @@ public class Main {
                     case "/" -> result = intFirstNumber / intSecondNumber;
                 }strResult = ArabicToRoman.convertArabicToRoman(result);
                 System.out.println(firstNumber+" "+operator+" "+secondNumber+" = "+strResult);
+
+            //------------------- calculation with arabic numerals --------------------
 
             }else if(firstNumber.matches("[0-9]+") && secondNumber.matches("[0-9]+")){
                 switch (operator) {
@@ -83,6 +101,9 @@ public class Main {
                         }
                     }
                 }System.out.println(firstNumber+" "+operator+" "+secondNumber+" = "+result);
+
+            //------------------- checking if wrong type entered ---------------------
+
             } else{
                 try {
                     throw new WrongTypeException("Окуганды билесинби? Калькулятор бир эле учурда араб же рим " +
